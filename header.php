37caae4fc286daa8ef16c20727534a64d2adb8ff
Jared Novack <jared@upstatement.com>
2016-04-19 19:14:22 -0400

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 /*
  * Third party plugins that hijack the theme will call wp_head() to get the header template.
  * We use this to start our output buffer and render into the view/page-plugin.twig template in footer.php
+ *
+ * If you're not using a plugin that requries this behavior (ones that do include Events Calendar Pro and 
+ * WooCommerce) you can delete this file.
  */
 $GLOBALS['timberContext'] = Timber::get_context();
 ob_start();
