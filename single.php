04aa1dad0d4dfa3553fab719b454063c64ca4bda
Connor Burton <connorjburton@gmail.com>
2016-05-04 20:22:33 +0100

    Switching deprecated function

--- single.php
+++ single.php
@@ -12,7 +12,7 @@
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
-$context['comment_form'] = TimberHelper::get_comment_form();
+$context['comment_form'] = comment_form();
 
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
