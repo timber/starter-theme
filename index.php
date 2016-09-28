06142d2502669f4d3ae9bf481e3d853417d0bb02
mgiulio <giulio.mainardi@gmail.com>
2016-09-28 16:31:13 +0200

    Timber activation test with 'template_include filter'

--- index.php
+++ index.php
@@ -13,10 +13,6 @@
  * @since   Timber 0.1
  */
 
-if ( ! class_exists( 'Timber' ) ) {
-	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-	return;
-}
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $context['foo'] = 'bar';
