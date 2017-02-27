a133dced6063038498de53aa0bdfe2fefdff0b1a
Dan Burzo <danburzo@gmail.com>
2017-02-27 16:28:35 +0200

    Added theme support for HTML5 features; fixes #40

--- functions.php
+++ functions.php
@@ -20,6 +20,7 @@ class StarterSite extends TimberSite {
 		add_theme_support( 'post-formats' );
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'menus' );
+		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
 		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
