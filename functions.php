b5ab962d7019e02431ae379f315b7d46f4ce289f
Jared Novack <jarednova@upstatement.com>
2019-07-04 11:54:39 -0400

    Include pattern for loading Timber via Composer

--- functions.php
+++ functions.php
@@ -8,8 +8,23 @@
  * @since   Timber 0.1
  */
 
+/**
+ * If you are installing Timber as a Composer dependency in your theme, you'll need this block
+ * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
+ * plug-in, you can safely delete this block. 
+ */
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
+if ( file_exists($composer_autoload) ) {
+	require_once( $composer_autoload );
+	$timber = new Timber\Timber();
+}
 
+/**
+ * This ensures that Timber is loaded and available as a PHP class.
+ * If not, it gives an error message to help direct developers on where to activate
+ */
 if ( ! class_exists( 'Timber' ) ) {
+
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
 	});
