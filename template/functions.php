795eec6c97387abf06088e9d57e58147b2ef267f
Viktor Szépe <viktor@szepe.net>
2020-06-03 04:05:52 +0000

    Fix paths harder

--- template/functions.php
+++ template/functions.php
@@ -44,7 +44,7 @@ if ( ! class_exists( 'Timber' ) ) {
 /**
  * Sets the directories (inside your theme) to find .twig files
  */
-Timber::$dirname = array( 'template/templates', 'template/views' );
+Timber::$dirname = array( '../templates', '../views' );
 
 /**
  * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
