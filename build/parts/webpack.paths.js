fb72c1ceed5b67776df59e9173966dfacb812a2b
Kyle Tozer <contact@kyletozer.com>
2020-06-09 22:40:58 -0400

    fixed src path in paths part

--- build/parts/webpack.paths.js
+++ build/parts/webpack.paths.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const root = path.resolve(__dirname, '../..');
-const src = path.join(__dirname, 'src');
+const src = path.join(root, 'src');
 const build = path.join(root, 'static');
 const output = path.join(build, 'scripts');
 
