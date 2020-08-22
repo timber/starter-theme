e31dafd8c61d64b804a06978c4822fd03bb20ddc
Jameel Moses <hello@jameelmoses.com>
2020-08-22 17:04:40 -0400

    Update single.php with new Timber\Post()

--- theme/single.php
+++ theme/single.php
@@ -10,7 +10,7 @@
  */
 
 $context         = Timber::context();
-$timber_post     = Timber::query_post();
+$timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 
 if ( post_password_required( $timber_post->ID ) ) {
