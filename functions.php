7b77b2d08a1782d6c5535fd6a2337fcbc2695458
Jared Novack <jared@upstatement.com>
2020-09-12 13:57:39 -0400

    Merge back of a few recent tweaks/changes into 1.x

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
  * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
  * plug-in, you can safely delete this block.
  */
-$composer_autoload = __DIR__ . '/vendor/autoload.php';
+$composer_autoload = dirname( __DIR__ ) . '/vendor/autoload.php';
 if ( file_exists( $composer_autoload ) ) {
 	require_once $composer_autoload;
 	$timber = new Timber\Timber();
@@ -35,7 +35,7 @@ if ( ! class_exists( 'Timber' ) ) {
 	add_filter(
 		'template_include',
 		function( $template ) {
-			return get_stylesheet_directory() . '/static/no-timber.html';
+			return dirname( get_stylesheet_directory() ) . '/static/no-timber.html';
 		}
 	);
 	return;
