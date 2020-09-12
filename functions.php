9f7e3781b9929432f9dcc58f81d41e36f569f42e
Jared Novack <jared@upstatement.com>
2020-09-12 14:09:22 -0400

    is the dirname giving it troubles?

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
  * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
  * plug-in, you can safely delete this block.
  */
-$composer_autoload = dirname( __DIR__ ) . '/vendor/autoload.php';
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $composer_autoload ) ) {
 	require_once $composer_autoload;
 	$timber = new Timber\Timber();
