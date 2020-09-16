cc595aa673ab2353367f6dd3a23d3ccf5e007ba5
Viktor Szépe <viktor@szepe.net>
2020-09-16 02:53:24 +0000

    Copy db.php in place during test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+copy(
+    dirname( __DIR__ ) . '/vendor/automattic/wordbless/src/dbless-wpdb.php',
+    dirname( __DIR__ ) . '/wordpress/wp-content/db.php'
+);
+
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 \WorDBless\Load::load();
