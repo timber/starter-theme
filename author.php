d8b4a571e1639b7e32ed68d10016856ac3e07c40
Michal Slepko <michal.slepko@gmail.com>
2022-04-05 22:18:38 +0100

    Update author.php
    Updated how author details are retrieved to fix Fatal error
    
    ```
    PHP Fatal error:  Uncaught ArgumentCountError: Too few arguments to function Timber\\PostQuery::__construct(), 0 passed in author.php on line 15 and exactly 1 expected in vendor/timber/timber/lib/PostQuery.php:85
    ```

--- author.php
+++ author.php
@@ -14,8 +14,11 @@ global $wp_query;
 $context          = Timber::context();
 $context['posts'] = new Timber\PostQuery();
 if ( isset( $wp_query->query_vars['author'] ) ) {
-	$author            = new Timber\User( $wp_query->query_vars['author'] );
-	$context['author'] = $author;
-	$context['title']  = 'Author Archives: ' . $author->name();
+	$factory = new Timber\Factory\UserFactory();
+        $author = $factory->from((int) $wp_query->query_vars['author']);
+	if ($author) {
+		$context['author'] = $author;
+		$context['title']  = 'Author Archives: ' . $author->name();
+	}
 }
 Timber::render( array( 'author.twig', 'archive.twig' ), $context );
